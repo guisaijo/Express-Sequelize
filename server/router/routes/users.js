8e3b3699d6b18336c985441dcfe3977e86c7b2f5
guisaijo <guilherme.saijo@rivendel.com.br>
2018-05-28 13:49:54 -0300

    Validate Json with Express-Validator

--- server/router/routes/users.js
+++ server/router/routes/users.js
@@ -1,65 +1,115 @@
 'use strict';
 
+const { checkSchema, validationResult } = require('express-validator/check');
+
 module.exports = (app, db) => {
 
   // GET health
   app.get('/health', (req, res) => {
-    res.status(200).send('OK');
+    return res.status(200).send('OK');
   });
 
   // GET all users
   app.get('/users', (req, res) => {
-    db.users.findAll()
-      .then(users => {
-        res.json(users);
-      });
+    db.users.findAll().then(users => {
+      return res.json(users);
+    });
   });
 
   // GET one user by id
   app.get('/user/:id', (req, res) => {
-    const id = req.params.id;
-    db.users.find({
-      where: { id: id}
-    })
-      .then(user => {
-        res.json(user);
+      const id = req.params.id;
+      if (id.length != 36){
+        return res.status(422).json({'Errors': 'ID must have 36 characters'});
+      }
+      db.users.find({
+        where: { id: id}
+      }).then(user => {
+        return res.json(user);
       });
   });
 
   // POST single user
-  app.post('/user', (req, res) => {
-    const name = req.body.name;
-    const age = req.body.age;
-    db.users.create({
-      name: name,
-      age: age
-    }).then(newUser => {
-      res.json(newUser);
-    })
+  app.post('/user', checkSchema({
+      name: {
+        in: ['body'],
+        exists: {
+          errorMessage: 'Name is empty!'
+        }
+      },
+      age: {
+        in: ['body'],
+        errorMessage: 'Age is not a number!',
+        isInt: true,
+        exists: {
+          errorMessage: 'Age is empty!'
+        }
+      }
+    }), (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ 'Errors': errors.array() });
+      }
+
+      const name = req.body.name;
+      const age = req.body.age;
+
+      db.users.create({
+        name: name,
+        age: age
+      }).then(newUser => {
+        return res.json(newUser);
+      })
   });
 
   // PATCH single user
-  app.patch('/user/:id', (req, res) => {
-    const id = req.params.id;
-    const updates = req.body;
-    db.users.find({
-      where: { id: id }
-    })
-      .then(user => {
-        return user.updateAttributes(updates)
-      }).then(updatedUser => {
-        res.json(updatedUser);
-      });
+  app.patch('/user/:id', checkSchema({
+      name: {
+        in: ['body'],
+        exists: {
+          errorMessage: 'Name is empty!'
+        }
+      },
+      age: {
+        in: ['body'],
+        errorMessage: 'Age is not a number!',
+        isInt: true,
+        exists: {
+          errorMessage: 'Age is empty!'
+        }
+      }
+    }), (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({ 'Errors': errors.array() });
+      }
+
+      const id = req.params.id;
+      if (id.length != 36){
+        return res.status(422).json({'Errors': 'ID must have 36 characters'})
+      }
+
+      const updates = req.body;
+      db.users.find({
+        where: { id: id }
+      }).then(user => {
+          return user.updateAttributes(updates)
+        }).then(updatedUser => {
+          return res.json(updatedUser);
+        });
   });
 
   // DELETE single user
   app.delete('/user/:id', (req, res) => {
     const id = req.params.id;
+    if (id.length != 36){
+      return res.status(422).json({'Errors': 'ID must have 36 characters'})
+    }
+
     db.users.destroy({
       where: { id: id }
-    })
-      .then(deletedUser => {
-        res.json(deletedUser);
-      });
+    }).then(deletedUser => {
+        return res.json(deletedUser);
+    });
   });
 };
